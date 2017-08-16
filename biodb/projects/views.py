2a82ffb263724ad012dbf2b0584af481410da225
admin <admin>
2017-08-16 14:46:14 +0200

    Refactoring of get function added get_queryset function

--- biodb/projects/views.py
+++ biodb/projects/views.py
@@ -25,26 +25,24 @@ class TagsListView(LoginRequiredMixin, ListView):
 
     def get(self, request, project_name, *args, **kwargs):
         """A base view for displaying a list of objects."""
+        # check if project exists
+        try:
+            project = Project.objects.get(name=project_name)
+            # add project to view attributes
+            self.project = project
+        except Project.DoesNotExist:
+            raise Http404
+        return super(TagsListView, self).get(self, request, project_name, *args, **kwargs)
 
-        queryset = self.get_queryset()
-        # self.object_list = Tag.objects.filter(project__name=project_name)
-        self.object_list = queryset.filter(project__name=project_name)
-        allow_empty = self.get_allow_empty()
+    def get_queryset(self):
+        """
+        Overwrite orginal qs and add filtering by project_name
+        """
+        # original queryset
+        qs = super().get_queryset()
 
-        if not allow_empty:
-            # When pagination is enabled and object_list is a queryset,
-            # it's better to do a cheap query than to load the unpaginated
-            # queryset in memory.
-            if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
-                is_empty = not self.object_list.exists()
-            else:
-                is_empty = len(self.object_list) == 0
-            if is_empty:
-                raise Http404(_("Empty list and '%(class_name)s.allow_empty' is False.") % {
-                    'class_name': self.__class__.__name__,
-                })
-        context = self.get_context_data()
-        return self.render_to_response(context)
+        # return filtered qs by project
+        return qs.filter(project=self.project)
 
 
 class TagCreateView(LoginRequiredMixin, CreateView):
@@ -60,16 +58,12 @@ class TagCreateView(LoginRequiredMixin, CreateView):
         return super(TagCreateView, self).get(self, request, project_name, *args, **kwargs)
 
     def form_valid(self, form):
-        if self.args:
-            project_name = self.args[0]
-            try:
-                project = Project.objects.get(name=project_name)
-                form.instance.project = project
-            except Project.DoesNotExist:
-                raise Http404
-        else:
-            raise ValidationError
-
+        project_name = self.args[0]
+        try:
+            project = Project.objects.get(name=project_name)
+            form.instance.project = project
+        except Project.DoesNotExist:
+            raise Http404
         return super(TagCreateView, self).form_valid(form)
 
 
