4c9d85b7ce3fb11dcdaefd7946e80c86f6538693
prusm <admin>
2017-08-17 14:59:28 +0200

    added unit tests for tag edit view

--- biodb/projects/tests/test_views.py
+++ biodb/projects/tests/test_views.py
@@ -59,7 +59,7 @@ class TagsListViewTestCase(FunctionalTest):
         response = self.client.get("/projects/project_1/tags/")
         self.assertEqual(response.status_code, 403)
 
-    def test_get_status_200_for_not_existed_project_name(self):
+    def test_get_status_404_for_not_existed_project_name(self):
         self.login_default_user()
         response = self.client.get("/projects/random_project/tags/")
         self.assertEqual(response.status_code, 404)
@@ -89,12 +89,12 @@ class TagCreateViewTestCase(FunctionalTest):
             "/projects//tags/create/", {'name': 'tag_name'})
         self.assertEqual(response.status_code, 404)
 
-    def test_get_return_404_for_non_exist_project(self):
+    def test_get_return_404_for_not_created_project(self):
         self.login_default_user()
         response = self.client.get("/projects/not_exist/tags/create/")
         self.assertEqual(response.status_code, 404)
 
-    def test_get_200_status_for_two_tags_with_the_same_name(self):
+    def test_get_302_status_for_two_tags_with_the_same_name(self):
         # expected rediect stsus 302
         self.login_default_user()
         proj1 = Project.objects.create(name="project_1")
@@ -103,3 +103,67 @@ class TagCreateViewTestCase(FunctionalTest):
         response = self.client.post(
             "/projects/project_1/tags/create/", {'name': 'same_tag'})
         self.assertEqual(response.status_code, 200)
+
+
+class TagEditViewTest(FunctionalTest):
+    def test_renders_given_template(self):
+        proj1 = Project.objects.create(name="project_1")
+        tag1 = Tag.objects.create(name='Tag1', project=proj1)
+        self.login_default_user()
+        response = self.client.get(f"/projects/project_1/tags/{tag1.id}/edit/")
+        self.assertTemplateUsed(response, "projects/tag_update_form.html")
+
+    def test_login_requirement(self):
+        proj1 = Project.objects.create(name="project_1")
+        tag1 = Tag.objects.create(name='Tag1', project=proj1)
+        response = self.client.get(f"/projects/project_1/tags/{tag1.id}/edit/")
+        # New in Django 1.11. Imstead of LoginRequiredMixin using method decorators
+        # It redirects to permited page, expected status code 302
+        self.assertEqual(response.status_code, 302)
+        # added extra asssertion to check redirect page
+        self.assertTemplateUsed('accounts/login.html')
+
+    def test_get_return_404_for_no_tagid(self):
+        self.login_default_user()
+        proj1 = Project.objects.create(name="project_1")
+        response = self.client.post(
+            "/projects/project_1/tags//edit/", {'name': 'tag_name'})
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_return_404_for_not_created_tagid(self):
+        self.login_default_user()
+        proj1 = Project.objects.create(name="project_1")
+        response = self.client.post(
+            f"/projects/project_1/tags/12345/edit/", {'name': 'tag_name'})
+        self.assertEqual(response.status_code, 404)
+
+    def test_tag_after_editing_in_correct_project(self):
+        proj1 = Project.objects.create(name="project_1")
+        self.login_default_user()
+        tag1, created = Tag.objects.get_or_create(name='Tag1', project=proj1)
+        response = self.client.post(
+            f"/projects/project_1/tags/{tag1.id}/edit/", {'name': 'tag1_name'})
+        # Check if status is correct and template
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('/projects/project_1/tags/')
+
+    def test_get_status_302_for_editing_tag_name_that_already_exists(self):
+        proj1 = Project.objects.create(name="project_1")
+        self.login_default_user()
+        tag1, created = Tag.objects.get_or_create(name='Tag1', project=proj1)
+        tag2, created = Tag.objects.get_or_create(name='Tag2', project=proj1)
+        response = self.client.post(
+            f"/projects/project_1/tags/{tag1.id}/edit/", {'name': 'Tag2'})
+        # Check if redirected corectly to the same page
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('/projects/project_1/tags/{tag1.id}/edit/')
+
+    def test_get_cutoff_tagname_for_to_long_edited_tag_name(self):
+        proj1 = Project.objects.create(name="project_1")
+        self.login_default_user()
+        tag1, created = Tag.objects.get_or_create(name='Tag1', project=proj1)
+        response = self.client.post(
+            f"/projects/project_1/tags/{tag1.id}/edit/", {'name': 'more_than_20_characters_string'})
+        # Check if succesfully posted request
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(tag1.name, 'more_than_20_characters_string')
