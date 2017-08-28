5238797d892c7990bf5397598cd7d2b8126f7dc2
prusm <admin>
2017-08-28 08:48:24 +0200

    added easy_pdf app and satic root

--- biodb/biodb/settings.py
+++ biodb/biodb/settings.py
@@ -17,6 +17,7 @@ from .email_settings import *
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = (
     #'tags',
     'guardian',
     'ckeditor',
+    'easy_pdf',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -110,7 +112,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 
 # add for userena app
 AUTHENTICATION_BACKENDS = (
