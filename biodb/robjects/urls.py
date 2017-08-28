0b6900d2047c432d775e4753dac7685ed991024b
prusm <admin>
2017-08-28 10:02:50 +0200

    added excel raport url

--- biodb/robjects/urls.py
+++ biodb/robjects/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
-from robjects.views import robjects_list_view, SearchRobjectsView, RobjectDetailView
+from robjects.views import robjects_list_view
+from robjects.views import SearchRobjectsView
+from robjects.views import RobjectDetailView
+from robjects.views import robjects_export_to_excel_view
 
 app_name="robjects"
 urlpatterns = [
@@ -8,4 +11,5 @@ urlpatterns = [
     url(r"^$", robjects_list_view, name="robjects_list"),
     url(r'^(?P<pk>[0-9]+)/details/$',
         RobjectDetailView.as_view(), name='robject_details'),
+    url(r'^(?P<pk>[0-9]+)/excel$', robjects_export_to_excel_view, name='export_to_excel')
 ]
