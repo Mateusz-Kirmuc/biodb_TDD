ae5d11b0d43fdc2f128b6556af4b9c0286c08ebf
prusm <admin>
2017-08-28 15:37:38 +0200

    added excel tests

---
+++ biodb/functional_tests/test_user_generates_excel.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from functional_tests.base import FunctionalTest
+from projects.models import Project
+from robjects.models import Robject
+from robjects.models import Tag
+from django.contrib.auth.models import User
+from django.test import tag
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait as wait
+
+
+@tag('slow')
+class UserGeneratesExcel(FunctionalTest):
+    def test_user_checks_output_file(self):
+        # Create sample project and robject
+        usr, proj = self.project_set_up_using_default_data()
+
+        # Create sample robjects basic informations.
+        robj1 = Robject.objects.create(
+            author=usr, project=proj, name="robject_1")
+
+        # User goes directly to robject detail page
+        self.browser.get(
+            self.live_server_url +
+            f"/projects/{proj.name}/robjects/{robj1.id}/excel/")
