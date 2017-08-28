0897da185bfa68eab67e5ee678cf57a91240bde2
prusm <admin>
2017-08-28 09:04:07 +0200

    added url for pdf raports

--- biodb/robjects/urls.py
+++ biodb/robjects/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
-from robjects.views import robjects_list_view, SearchRobjectsView, RobjectDetailView
+from robjects.views import RobjectDetailView
+from robjects.views import robjects_list_view
+from robjects.views import SearchRobjectsView
+from robjects.views import robjects_pdf_view
 
 app_name="robjects"
 urlpatterns = [
-    url(r"^search/$",
+    url(r'^search/$',
         SearchRobjectsView.as_view(), name="search_robjects"),
-    url(r"^$", robjects_list_view, name="robjects_list"),
+    url(r'^$', robjects_list_view, name="robjects_list"),
     url(r'^(?P<pk>[0-9]+)/details/$',
         RobjectDetailView.as_view(), name='robject_details'),
+    url(r'^(?P<pk>[0-9]+)/raport_pdf/$', robjects_pdf_view, name='raport_pdf'),
 ]
