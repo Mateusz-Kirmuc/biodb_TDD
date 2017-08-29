7ec613fc057a6ff71b479c0913cef08f67b83362
prusm <admin>
2017-08-29 15:28:18 +0200

    to d ofunctional test

--- biodb/functional_tests/test_user_generates_excel.py
+++ biodb/functional_tests/test_user_generates_excel.py
@@ -9,26 +9,23 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait as wait
+import time
+from selenium.webdriver.common.keys import Keys
 
 
 @tag('slow')
 class UserGeneratesExcel(FunctionalTest):
     def test_user_checks_output_file(self):
         # Create sample project and robject
+
         usr, proj = self.project_set_up_using_default_data()
 
         # Create sample robjects basic informations.
         robj1 = Robject.objects.create(
             author=usr, project=proj, name="robject_1")
-
-        # User goes directly to robject detail page
-        self.browser.get(
-            self.live_server_url +
-            f"/projects/{proj.name}/robjects/{robj1.id}/excel/")
-
-
-        try:
-            alert = self.browser.switch_to_alert()
-            alert.accept()
-        except:
-            print ("no alert to accept")
+        # TODO: how to save this file, selenium is not
+        # working with object other than htmls
+        # get this url
+            # self.browser.get(
+            #     self.live_server_url +
+            #     f"/projects/{proj.name}/robjects/{robj1.id}/excel/")
