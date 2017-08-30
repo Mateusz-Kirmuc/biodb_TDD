600e226f14e66d3504b74f46956a3ecc729935d0
prusm <admin>
2017-08-30 16:08:45 +0200

    added url for multiple robjects pdf

--- biodb/robjects/urls.py
+++ biodb/robjects/urls.py
@@ -3,6 +3,7 @@ from robjects.views import RobjectDetailView
 from robjects.views import robjects_list_view
 from robjects.views import SearchRobjectsView
 from robjects.views import robjects_pdf_view
+from robjects.views import robjects_selected_pdf_view
 
 app_name="robjects"
 urlpatterns = [
@@ -12,4 +13,5 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/details/$',
         RobjectDetailView.as_view(), name='robject_details'),
     url(r'^(?P<pk>[0-9]+)/raport_pdf/$', robjects_pdf_view, name='raport_pdf'),
+    url(r'^raport_pdf/$', robjects_selected_pdf_view, name='raport_pdf'),
 ]
