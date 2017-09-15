ca1939c7c33c0f61da81dae60351c18af1adcd02
prusm <admin>
2017-09-15 11:38:33 +0200

    TagListView

--- biodb/projects/tests/test_models.py
+++ biodb/projects/tests/test_models.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.db import models
 from django.test import TestCase
 from projects.models import Project
+from projects.models import Tag
 
 
 class ProjectModelTestCase(TestCase):
@@ -28,3 +29,27 @@ class ProjectModelTestCase(TestCase):
         Project.objects.create(name="PROJECT_1")
         with self.assertRaises(IntegrityError):
             Project.objects.create(name="PROJECT_1")
+
+class TagModelTestCase(TestCase):
+
+    def test_tag_name_uniqueness(self):
+        proj = Project.objects.create(name="PROJECT_1")
+        Tag.objects.create(name="Tag1", project=proj)
+        with self.assertRaises(IntegrityError):
+            Tag.objects.create(name="Tag1", project=proj)
+
+    def test_get_absolute_url(self):
+        proj = Project.objects.create(name="top_secret")
+        tag = Tag.objects.create(name="Tag1", project=proj)
+        self.assertEqual(tag.get_absolute_url(),
+                         f"/projects/top_secret/tags/")
+
+    def test_tag_str(self):
+        proj = Project.objects.create(name="top_secret")
+        tag = Tag.objects.create(name="Tag1", project=proj)
+        self.assertEqual(tag.__str__(), "Tag1")
+
+    def test_tag_unicode(self):
+        proj = Project.objects.create(name="top_secret")
+        tag = Tag.objects.create(name="Tag1", project=proj)
+        self.assertEqual(tag.__unicode__(), "Tag1")
--- biodb/projects/tests/test_views.py
+++ biodb/projects/tests/test_views.py
@@ -1,6 +1,7 @@
-# from django.contrib.auth.models import User
-# from django.test import TestCase
+from django.http import Http404
 from projects.models import Project
+from robjects.models import Robject
+from projects.models import Tag
 from unit_tests.base import FunctionalTest
 
 
@@ -26,3 +27,44 @@ class ProjectListViewTestCase(FunctionalTest):
     def test_login_requirement(self):
         response = self.client.get("/projects/")
         self.assertEqual(response.status_code, 403)
+
+
+class TagsListViewTestCase(FunctionalTest):
+    def test_renders_given_template(self):
+        proj1 = Project.objects.create(name="project_1")
+        self.login_default_user()
+        response = self.client.get("/projects/project_1/tags/")
+        self.assertTemplateUsed(response, "tags/tag_list.html")
+
+    def test_tags_in_correct_project(self):
+        proj1 = Project.objects.create(name="project_1")
+        proj2 = Project.objects.create(name="project_2")
+        self.login_default_user()
+        tag1 = Tag.objects.get_or_create(name='Tag1', project=proj1)
+        tag2 = Tag.objects.get_or_create(name='Tag2', project=proj2)
+        response = self.client.get("/projects/project_1/tags/")
+        self.assertIn(tag1[0], response.context['tag_list'])
+        self.assertNotIn(tag2[0], response.context['tag_list'])
+
+    def test_allow_empty_tag_list(self):
+        proj1 = Project.objects.create(name="project_1")
+        self.login_default_user()
+        response = self.client.get("/projects/project_1/tags/")
+        self.assertEqual(0, len(response.context['tag_list']))
+
+    def test_login_requirement(self):
+        proj1 = Project.objects.create(name="project_1")
+        response = self.client.get("/projects/project_1/tags/")
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_status_404_for_not_existed_project_name(self):
+        self.login_default_user()
+        response = self.client.get("/projects/random_project/tags/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_status_404_for_no_project_name(self):
+        self.login_default_user()
+        response = self.client.get("/projects//tags/")
+
+
+        self.assertEqual(response.status_code, 404)
